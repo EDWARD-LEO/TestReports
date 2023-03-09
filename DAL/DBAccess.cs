304433ce1e32806b07c85ed2d217f530edd79b12
JhonFM <jfranciaminaya@gmail.com>
2023-03-09 12:44:14 -0500

    Agregar archivos de proyecto.

---
+++ DAL/DBAccess.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+
+namespace DAL
+{
+    public class DBAccess
+    {
+        private SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["sisreports"].ConnectionString);
+
+        public SqlConnection getConexion()
+        {
+            return this.conn;
+        }
+
+        public void conectar()
+        {
+            if (this.conn.State == ConnectionState.Closed)
+            {
+                this.conn.Open();
+            }
+        }
+
+        public void desconectar()
+        {
+            if (this.conn.State == ConnectionState.Open)
+            {
+                this.conn.Close();
+            }
+        }
+
+        public DataTable getDataFrom(string spu, int estado)
+        {
+            DataTable dt = new DataTable();
+            conectar();
+
+            SqlCommand sqlCommand = new SqlCommand(spu, getConexion());
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+            sqlCommand.Parameters.AddWithValue("@estado", estado);
+            dt.Load(sqlCommand.ExecuteReader());
+
+            desconectar();
+            return dt;
+        }
+    }
+}
